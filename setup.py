31fec23e2ddc5cd61e3ec894c5eb9ddb589842e5
Richard Munoz <rmunoz@iown1.net>
2017-05-31 15:20:04 -0400

    conditionally require grpcio-tools during setup

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ from setuptools.command.develop import develop
 import os
 
 
+try:
+    from grpc.tools import command
+except ImportError:
+    build_requires = ['grpcio-tools']
+else:
+    build_requires = []
+
+
 class PackgeProtoBuilderMixin(object):
     def build_package_protos(self):
         from grpc.tools import command
@@ -32,10 +40,7 @@ setup(
     packages=['tensorflow_serving_python', 'tensorflow_serving_python.protos',
               'tensorflow_serving_python.tensor_utils'],
     package_dir={'': 'src'},
-    setup_requires=[
-        'cython',
-        'grpcio-tools'
-    ],
+    setup_requires=build_requires,
     install_requires=[
         'cython',
         'grpcio',
